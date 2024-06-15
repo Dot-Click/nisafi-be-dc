1cadbf14f8ce69d816dafd817b7614b9abddfba9
Ahmed Hassan <ah2k.dev@gmail.com>
2024-06-15 10:06:20 +0500

    work

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -24,7 +24,7 @@ const register = async (req, res) => {
       email,
       password,
       role,
-      // phone,
+      phone,
     });
     newUser.save();
     return SuccessHandler("User created successfully", 200, res);
